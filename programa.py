40a0701c4a05fe0b34155ea66ac965c9668681bf
duque-maria <mariangelduque@colegioguanenta.edu.co>
2025-02-06 15:48:00 -0500

    Se modificó el README y se agregó el diagrama de flujo

---
+++ programa.py
@@ -0,0 +1,11 @@
+print("····························")
+print("Minicalculadora Guanentina")
+print("····························")
+print(Minicalculadora Guanentina
+······································
+ 1-- sumar (x+y)
+  2-- restar (x-y)
+3-- multiplicar (x*y)
+4-- división (x/y)
+5-- potencia (x**y)
+6-- logaritmo (math.log(x,y)))
