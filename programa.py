e38c510b4b9d69c2c610951ba9a536473b007a0c
duque-maria <mariangelduque@colegioguanenta.edu.co>
2025-02-06 18:00:54 -0500

    se agregó el programa

--- programa.py
+++ programa.py
@@ -1,11 +1,42 @@
-print("····························")
+import math 
+
+# input
+print("······························")
 print("Minicalculadora Guanentina")
-print("····························")
-print(Minicalculadora Guanentina
-······································
- 1-- sumar (x+y)
-  2-- restar (x-y)
-3-- multiplicar (x*y)
-4-- división (x/y)
-5-- potencia (x**y)
-6-- logaritmo (math.log(x,y)))
+print("······························")
+
+print("1-- sumar (x+y)")
+print("2-- restar (x-y)")
+print("3-- multiplicar (x*y)")
+print("4-- división (x/y)")
+print("5-- potencia (x**y)")
+print("6-- logaritmo (math.log(x,y))")
+
+
+x = int(input("Introducir el primer numero: "))
+y = int(input("Introducir el segundo numero: "))
+opc = int(input("Selecciona una operación: "))
+
+# processing
+if opc == 1:
+    r= (x+y)
+else:
+    if opc== 2:
+        r= (x-y) # output
+    else:
+        if opc == 3:
+            r= (x*y) # ouput
+        else:
+            if opc == 4:
+                r= (x/y) # output
+            else: 
+                if opc == 5:
+                    r= (x**y) # output
+                else:
+                    if opc== 6:
+                        r= (math.log(x,y)) # output
+                    else:
+                        r= "Opción no válida" # output
+
+# output
+print("Resultado: " + str(r))
